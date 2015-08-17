d037479a962130ca25e99bb69dddcc2baeda9f16
tone <anthonyschwartzcode@gmail.com>
2015-08-17 14:39:03 -0700

    flashing

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var parent = document.getElementsByTagName("body")[0];
+var randomRgb = Math.round(Math.random()*255);
+var newBox = document.getElementsByTagName("div");
 
 
 function checkerBoard() {
@@ -8,14 +10,14 @@ function checkerBoard() {
       newBox.style.width = "11.1%";
       newBox.style.float = "left";
       newBox.style.paddingBottom = "11.1%";
-      newBox.style.backgroundColor = "black";
+      newBox.style.backgroundColor = randomColor();
       parent.appendChild(newBox)
     } else {
       var newBox = document.createElement("div");
       newBox.style.width = "11.1%";
       newBox.style.float = "left";
       newBox.style.paddingBottom = "11.1%";
-      newBox.style.backgroundColor = "red";
+      newBox.style.backgroundColor = randomColor();
       parent.appendChild(newBox)
     }
 
@@ -23,3 +25,20 @@ function checkerBoard() {
 }
 
 checkerBoard();
+
+
+function randomColor(){
+  var red = Math.round(Math.random()*255);
+  var green = Math.round(Math.random()*255);
+  var blue = Math.round(Math.random()*255);
+  return "rgb("+red+","+green+","+blue+")"
+  // return `rgb(${red},${green},${blue})` es6 concatination syntax
+}
+randomColor()
+
+function flashColor(){
+  for (var i = 0; i < newBox.length; i++) {
+    newBox[i].style.backgroundColor = randomColor()
+  }
+}
+var flash = setInterval(flashColor, 250);
